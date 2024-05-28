3785f1c5f6531b714a4e3be07e408f0d340bdccb
Maciej Lencewicz <maciej.lencewicz@gmail.com>
2024-05-28 14:59:48 +0200

    Implementacja funkcjonalności w zakładce punkty gastronomiczne

--- src/Pages/LocalInfo.jsx
+++ src/Pages/LocalInfo.jsx
@@ -1,35 +1,74 @@
-import { useNavigate } from "react-router-dom";
-
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { IoIosArrowBack } from "react-icons/io";
-
-const mockedLocal = {name: 'Index', address: 'ul. JakasTam 21/37', img: 'https://indeksklub.pl/gallery/gallery2.JPG'}
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+import { db } from "../firebase";
 
 function LocalInfo() {
-    const navigate = useNavigate();
+  const { localID } = useParams();
+  const navigate = useNavigate();
+  const [localData, setLocalData] = useState(null);
 
-    return (
-        <div className="flex items-center justify-center h-screen font-bold">
-            <div className="mt-10 bg-blue w-[95%] h-5/6 rounded-3xl">
-                <div className="bg-lightblue w-full rounded-t-3xl lg:text-2xl text-l flex justify-between items-center">
-                    <span className='p-3'>Informacja o punkcie gastronomicznym</span>
-                    <button onClick={() => navigate("/punkty-gastronomiczne")} className='flex items-center justify-center mx-3 duration-500 bg-blue hover:bg-opacity-50 rounded-md my-2 py-1 px-3'><div className='w-5 '> <IoIosArrowBack /> </div>Wróć</button>
-                </div>
-                <div className="flex flex-col items-center overflow-y-auto max-h-[85%] lg:text-2xl text-l mt-10">
-                    <div className="flex items-center justify-between lg:w-[40%] md:w-[55%] w-[70%] bg-lightblue bg-opacity-50 my-2 p-3">
-                        <spam>Nazwa lokalu: </spam>
-                        <spam>{mockedLocal.name} </spam>
-                    </div>
-                    <div className="flex items-center justify-between lg:w-[40%] md:w-[55%] w-[70%] bg-lightblue bg-opacity-50 my-2 p-3">
-                        <spam>Adres lokalu: </spam>
-                        <spam>{mockedLocal.address} </spam>
-                    </div>
-                    <div className="flex items-center justify-between lg:w-[40%] md:w-[55%] w-[70%] bg-[#FFFFFF] my-2 p-3">
-                        <img className="" src={mockedLocal.img}/>
-                    </div>
-                </div>
+  useEffect(() => {
+    const fetchLocalData = async () => {
+      try {
+        const db = getFirestore();
+        const localRef = doc(db, "restaurants", localID);
+        const localDoc = await getDoc(localRef);
+        if (localDoc.exists()) {
+          setLocalData(localDoc.data());
+        } else {
+          console.log("Nie znaleziono danych dla lokalu o ID:", localID);
+        }
+      } catch (error) {
+        console.error("Błąd podczas pobierania danych lokalu:", error);
+      }
+    };
+
+    fetchLocalData();
+  }, [localID]);
+
+  return (
+    <div className="flex items-center justify-center h-screen font-bold">
+      <div className="mt-10 bg-blue w-[95%] h-5/6 rounded-3xl">
+        <div className="bg-lightblue w-full rounded-t-3xl lg:text-2xl text-l flex justify-between items-center">
+          <span className="p-3">Informacja o punkcie gastronomicznym</span>
+          <button
+            onClick={() => navigate("/punkty-gastronomiczne")}
+            className="flex items-center justify-center mx-3 duration-500 bg-blue hover:bg-opacity-50 rounded-md my-2 py-1 px-3"
+          >
+            <div className="w-5">
+              <IoIosArrowBack />
             </div>
+            Wróć
+          </button>
         </div>
-    );
+        {localData ? (
+          <div className="flex flex-col items-center overflow-y-auto max-h-[85%] lg:text-2xl text-l mt-10">
+            <div className="flex items-center justify-between lg:w-[40%] md:w-[55%] w-[70%] bg-lightblue bg-opacity-50 my-2 p-3">
+              <span>Nazwa lokalu: </span>
+              <span>{localData.Name}</span>
+            </div>
+            <div className="flex items-center justify-between lg:w-[40%] md:w-[55%] w-[70%] bg-lightblue bg-opacity-50 my-2 p-3">
+              <span>Adres lokalu: </span>
+              <span>{localData.Location}</span>
+            </div>
+            <div className="flex items-center justify-between lg:w-[40%] md:w-[55%] w-[70%] bg-lightblue bg-opacity-50 my-2 p-3">
+              <span>Link do mapy: </span>
+              <span>{localData.MapLink} </span>
+            </div>
+            <div className="flex items-center justify-between lg:w-[40%] md:w-[55%] w-[70%] bg-[#FFFFFF] my-2 p-3">
+              <img className="" src={localData.ImageLink} alt="Zdjęcie lokalu" />
+            </div>
+          </div>
+        ) : (
+          <div className="flex items-center justify-center h-full">
+            <span>Ładowanie danych...</span>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default LocalInfo;
--- src/Pages/Locals.jsx
+++ src/Pages/Locals.jsx
@@ -1,58 +1,134 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { FaInfo } from "react-icons/fa6";
-import { AiOutlineCheck } from "react-icons/ai";
-import { AiOutlineClose } from "react-icons/ai";
+import { AiOutlineCheck, AiOutlineClose } from "react-icons/ai";
 
-const mockedLocals = [
-    { id: 0, name: 'Index'},
-    { id: 1, name: 'Finestra'},
-    { id: 2, name: 'Bar Agh Hnung'},
-    { id: 3, name: 'Forno Nero'},
-    { id: 4, name: 'Alora'},
-  ];
+import { db, collection, getDocs, doc, updateDoc, deleteDoc } from "../firebase";
+
+import Swal from "sweetalert2";
 
 function Locals() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [locals, setLocals] = useState([]);
 
-    const [locals, setLocals] = useState(mockedLocals);
+  useEffect(() => {
+    const fetchLocals = async () => {
+      const localsCollection = collection(db, "restaurants");
+      const localsSnapshot = await getDocs(localsCollection);
+      const localsList = localsSnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      setLocals(localsList.filter((local) => !local.IsAccepted));
+    };
 
-    useEffect ( () => {
-        setLocals(mockedLocals)
-    }, [])
+    fetchLocals();
+  }, []);
 
-    return (
-        <div className="flex items-center justify-center h-screen font-bold">
-            <div className="mt-10 bg-blue w-[95%] h-5/6 rounded-3xl ">
-                <div className="bg-lightblue w-full rounded-t-3xl p-3 lg:text-2xl text-l">
-                    Nowe zgłoszenia dotyczące punktów gastronomicznych
-                </div>
-                <div className="overflow-y-auto lg:max-h-[90%] max-h-[85%]">
-                    <table className="w-full mt-2 lg:text-xl text-md border-separate border-spacing-y-1">
-                        <thead className="bg-lightblue text-left">
-                        <tr>
-                            <th className="p-3 lg:w-[55%]">Nazwa punktu gastronomicznego</th>
-                            <th className="pl-5">Akcje</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {locals.map((local, index) => (
-                            <tr key={index} className="bg-lightblue bg-opacity-50">
-                                <td className="p-5">{local.name}</td>
-                                <td className="lg:flex items-center justify-between">
-                                    <button onClick={() => navigate("/punkty-gastronomiczne-info")} className='flex items-center duration-500 hover:bg-lightblue rounded-md my-2 p-3'><div className='w-5 '> <FaInfo /> </div>Informacje</button>
-                                    <button className='flex items-center duration-500 hover:bg-lightblue rounded-md my-2 p-3'><div className='w-5 '> <AiOutlineCheck /> </div>Akceptuj</button>
-                                    <button className='flex items-center duration-500 hover:bg-lightblue rounded-md my-2 p-3'><div className='w-5 '> <AiOutlineClose /> </div>Odrzuć</button>
-                                </td>
-                            </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+  const acceptLocal = async (id) => {
+    try {
+      const localDoc = doc(db, "restaurants", id);
+      await updateDoc(localDoc, { IsAccepted: true });
+      setLocals(locals.filter((local) => local.id !== id));
+      Swal.fire({
+        icon: "info",
+        title: "Zaakceptowano punkt",
+        color: "#2BA1F0",
+        confirmButtonColor: "#2BDFF0",
+        timer: 3000,
+      });
+    } catch (error) {
+      console.error("Błąd podczas akceptacji lokalu: ", error);
+    }
+  };
+
+  const rejectLocal = async (id) => {
+    const result = await Swal.fire({
+      title: "Czy chcesz odrzucić lokal?",
+      showCancelButton: true,
+      confirmButtonText: "Odrzuć",
+      cancelButtonText: `Anuluj`,
+      color: "#2BA1F0",
+      confirmButtonColor: "#2BDFF0",
+    });
+
+    if (result.isConfirmed) {
+      try {
+        await deleteDoc(doc(db, "restaurants", id));
+        setLocals(locals.filter((local) => local.id !== id));
+        Swal.fire({
+          title: "Odrzucono zgłoszenie!",
+          icon: "info",
+          color: "#2BA1F0",
+          confirmButtonColor: "#2BDFF0",
+          timer: 3000,
+        });
+      } catch (error) {
+        Swal.fire({
+          icon: "error",
+          title: "Błąd",
+          text: "Nie udało się odrzucić lokal",
+          color: "#2BA1F0",
+          confirmButtonColor: "#2BDFF0",
+          timer: 3000,
+        });
+      }
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center h-screen font-bold">
+      <div className="mt-10 bg-blue w-[95%] h-5/6 rounded-3xl">
+        <div className="bg-lightblue w-full rounded-t-3xl p-3 lg:text-2xl text-l">
+          Nowe zgłoszenia dotyczące punktów gastronomicznych
         </div>
-    )
+        <div className="overflow-y-auto lg:max-h-[90%] max-h-[85%]">
+          <table className="w-full mt-2 lg:text-xl text-md border-separate border-spacing-y-1">
+            <thead className="bg-lightblue text-left">
+              <tr>
+                <th className="p-3 lg:w-[55%]">Nazwa punktu gastronomicznego</th>
+                <th className="pl-5">Akcje</th>
+              </tr>
+            </thead>
+            <tbody>
+              {locals.map((local, index) => (
+                <tr key={index} className="bg-lightblue bg-opacity-50">
+                  <td className="p-5">{local.Name}</td>
+                  <td className="lg:flex items-center justify-between">
+                    <button
+                      onClick={() => navigate(`/punkty-gastronomiczne-info/${local.id}`)}
+                      className="flex items-center duration-500 hover:bg-lightblue rounded-md my-2 p-3"
+                    >
+                      <div className="w-5">
+                        <FaInfo />
+                      </div>
+                      Informacje
+                    </button>
+                    <button
+                      onClick={() => acceptLocal(local.id)}
+                      className="flex items-center duration-500 hover:bg-lightblue rounded-md my-2 p-3"
+                    >
+                      <div className="w-5">
+                        <AiOutlineCheck />
+                      </div>
+                      Akceptuj
+                    </button>
+                    <button
+                      onClick={() => rejectLocal(local.id)}
+                      className="flex items-center duration-500 hover:bg-lightblue rounded-md my-2 p-3"
+                    >
+                      <div className="w-5">
+                        <AiOutlineClose />
+                      </div>
+                      Odrzuć
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Locals
+export default Locals;
