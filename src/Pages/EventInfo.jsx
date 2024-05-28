a4a0f9b5cc911f2d8e7314968a84dcbeddd1e65c
Maciej Bartoszewski <maciejbartoszewski02@gmail.com>
2024-05-28 12:04:33 +0200

    implementacja wyswietlenia informacji o poprannym wydarzeniu

--- src/Pages/EventInfo.jsx
+++ src/Pages/EventInfo.jsx
@@ -1,54 +1,79 @@
-import { useNavigate } from "react-router-dom";
-
+import { useParams, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import { IoIosArrowBack } from "react-icons/io";
-
-const mockedEvent = {
-    name: 'Dzień WEEIA',
-    place: 'WEEIA',
-    date: '24 marca 2024',
-    description: 'Jest to wydarzenie, które umożliwia Wam zdobycie nowych umiejętności i zaznajomienie się z różnymi ofertami pracy i praktyk.',
-    map: 'link.do.mapy.pl',
-    img: 'https://mlodziwlodzi.pl/wp-content/uploads/420125575_877561351038343_6256364071254790138_n-1200x628.jpg'
-}
+import { getFirestore, collection, getDocs, getDoc, doc, updateDoc, deleteDoc } from "firebase/firestore";
 
 function EventInfo() {
-    const navigate = useNavigate();
+  const { eventID } = useParams();
+  const navigate = useNavigate();
+  const [eventData, setEventData] = useState(null);
 
-    return (
-        <div className="flex items-center justify-center h-screen font-bold">
-            <div className="mt-10 bg-blue w-[95%] h-5/6 rounded-3xl">
-                <div className="bg-lightblue w-full rounded-t-3xl lg:text-2xl text-l flex justify-between items-center">
-                    <span className='p-3'>Informacja o wydarzeniu</span>
-                    <button onClick={() => navigate("/wydarzenia")} className='flex items-center justify-center mx-3 duration-500 bg-blue hover:bg-opacity-50 rounded-md my-2 py-1 px-3'><div className='w-5'><IoIosArrowBack /></div>Wróć</button>
-                </div>
-                <div className="flex flex-col items-center overflow-y-auto max-h-[85%] lg:text-2xl text-l mt-6">
-                    <div className="flex items-center justify-between lg:w-[40%] md:w-[55%] w-[70%] bg-lightblue bg-opacity-50 my-2 p-3">
-                        <spam>Nazwa wydarzenia: </spam>
-                        <spam>{mockedEvent.name} </spam>
-                    </div>
-                    <div className="flex items-center justify-between lg:w-[40%] md:w-[55%] w-[70%] bg-lightblue bg-opacity-50 my-2 p-3">
-                        <spam>Miejsce wydarzenia: </spam>
-                        <spam>{mockedEvent.place} </spam>
-                    </div>
-                    <div className="flex items-center justify-between lg:w-[40%] md:w-[55%] w-[70%] bg-lightblue bg-opacity-50 my-2 p-3">
-                        <spam>Data wydarzenia: </spam>
-                        <spam>{mockedEvent.date} </spam>
-                    </div>
-                    <div className="flex items-center justify-between lg:w-[40%] md:w-[55%] w-[70%] bg-lightblue bg-opacity-50 my-2 p-3">
-                        <spam>Opis: </spam>
-                        <spam className='w-[80%]'>{mockedEvent.description} </spam>
-                    </div>
-                    <div className="flex items-center justify-between lg:w-[40%] md:w-[55%] w-[70%] bg-lightblue bg-opacity-50 my-2 p-3">
-                        <spam>Link do mapy: </spam>
-                        <spam>{mockedEvent.map} </spam>
-                    </div>
-                    <div className="flex items-center justify-between lg:w-[40%] md:w-[55%] w-[70%] bg-[#FFFFFF] my-2 p-3">
-                        <img className="" src={mockedEvent.img}/>
-                    </div>
-                </div>
+  useEffect(() => {
+    const fetchEventData = async () => {
+      try {
+        const db = getFirestore();
+        const eventRef = doc(db, "events", eventID);
+        const eventDoc = await getDoc(eventRef);
+        if (eventDoc.exists()) {
+          setEventData(eventDoc.data());
+        } else {
+          console.log("Nie znaleziono danych dla wydarzenia o ID:", eventID);
+        }
+      } catch (error) {
+        console.error("Błąd podczas pobierania danych wydarzenia:", error);
+      }
+    };
+
+    fetchEventData();
+  }, [eventID]);
+
+  return (
+    <div className="flex items-center justify-center h-screen font-bold">
+      <div className="mt-10 bg-blue w-[95%] h-5/6 rounded-3xl">
+        <div className="bg-lightblue w-full rounded-t-3xl lg:text-2xl text-l flex justify-between items-center">
+          <span className="p-3">Informacja o wydarzeniu</span>
+          <button
+            onClick={() => navigate("/wydarzenia")}
+            className="flex items-center justify-center mx-3 duration-500 bg-blue hover:bg-opacity-50 rounded-md my-2 py-1 px-3"
+          >
+            <div className="w-5">
+              <IoIosArrowBack />
             </div>
+            Wróć
+          </button>
         </div>
-    );
+        <div className="flex flex-col items-center overflow-y-auto max-h-[85%] lg:text-2xl text-l mt-6">
+          {eventData && (
+            <>
+              <div className="flex items-center justify-between lg:w-[40%] md:w-[55%] w-[70%] bg-lightblue bg-opacity-50 my-2 p-3">
+                <span>Nazwa wydarzenia: </span>
+                <span>{eventData.Name} </span>
+              </div>
+              <div className="flex items-center justify-between lg:w-[40%] md:w-[55%] w-[70%] bg-lightblue bg-opacity-50 my-2 p-3">
+                <span>Miejsce wydarzenia: </span>
+                <span>{eventData.Location} </span>
+              </div>
+              <div className="flex items-center justify-between lg:w-[40%] md:w-[55%] w-[70%] bg-lightblue bg-opacity-50 my-2 p-3">
+                <span>Data wydarzenia: </span>
+                <span>{eventData.Date} </span>
+              </div>
+              <div className="flex items-center justify-between lg:w-[40%] md:w-[55%] w-[70%] bg-lightblue bg-opacity-50 my-2 p-3">
+                <span>Opis: </span>
+                <span className="w-[80%] text-right">{eventData.Description} </span>
+              </div>
+              <div className="flex items-center justify-between lg:w-[40%] md:w-[55%] w-[70%] bg-lightblue bg-opacity-50 my-2 p-3">
+                <span>Link do mapy: </span>
+                <span>{eventData.MapLink} </span>
+              </div>
+              <div className="flex items-center justify-between lg:w-[40%] md:w-[55%] w-[70%] bg-[#FFFFFF] my-2 p-3">
+                <img className="" src={eventData.ImageLink} />
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default EventInfo;
